4967136428183b303e046e478e887973e637fe55
Alberto P Lopes <perdigao38@outlook.com>
2023-10-11 13:36:38 -0300

    trabalho 01

---
+++ lib/screens/recipes_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../../models/models.dart';
+import '../../api/mock_gastronomico_service.dart';
+import '../../components/components.dart';
+
+class RecipesScreen extends StatelessWidget {
+  final exploreService = MockGastronomicoService();
+  RecipesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: exploreService.getRecipes(),
+      builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return RecipesGridView(recipes: snapshot.data ?? []);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
